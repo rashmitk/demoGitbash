947b42b384bebb882b7877019413b43867913f7d
rashmitk <rashmi1tk@gmail.com>
2022-02-01 12:02:51 +0530

    first push of project

---
+++ src/GitBashDemo/Pgm001Test.java
@@ -0,0 +1,10 @@
+package GitBashDemo;
+
+public class Pgm001Test {
+
+	public static void main(String[] args) {
+		System.out.println("gitbash cmd line");
+
+	}
+
+}
---
+++ src/GitBashDemo/Pgm002Test.java
@@ -0,0 +1,8 @@
+package GitBashDemo;
+
+public class Pgm002Test {
+	public static void main(String[] args) {
+		System.out.println("Type here 2nd file");
+	}
+
+}
